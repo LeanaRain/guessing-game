181f930fae8af29bc156eda09a2bcaa834947b47
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2023-10-20 16:56:57 +0500

    init: start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minNum;
+        this.maxNum;
+        this.currentNum; //текущее значение
+    }
 
+    //устанавливаем диапазон чисел для угадывания
     setRange(min, max) {
-
+        this.minNum = 0;
+        this.maxNum = 100;
     }
 
     guess() {
-
+        this.currentNum = Math.round((this.minNum + this.maxNum) / 2);
+        return this.currentNum;
     }
 
+    //если число меньше 
     lower() {
-
+        this.maxNum = this.currentNum - 1; //уменьшаем максимальное значение диапазона
     }
-
+    //если число больше
     greater() {
-
+        this.minNum = this.currentNum + 1; //увеличиваем минимальное значение диапазона
     }
 }
 
